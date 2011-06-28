afb00f91392cd9590dd80159e1bccb48cc305f63
lashchenko <andrew.lashchenko@gmail.com>
2011-06-28 14:03:17 +0300

    ignore spacing in colors config file. added default background color - no need additional set it in config file.

--- colorparser.cpp
+++ colorparser.cpp
@@ -25,17 +25,17 @@ QString ColorParser::processString(QString string)
     foreach( QStringList list, config ) {
         if( string.indexOf(list.first()) != -1 ) {
             QString line;
-            line += "<div style=\"background-color:'" + list.at(1) + "';"
-                    + "color:'" + list.at(2) + "';" + "\">";
+            line += "<div style=\"background-color:'" + (list.at(1).trimmed().isEmpty() ? "#222" : list.at(1).trimmed()) + "';"
+                    + "color:'" + list.at(2).trimmed() + "';" + "\">";
             if( list.size() > 3 ) {
                 for( int i=3; i<list.size(); ++i ) {
-                    line += "<" + list.at(i) + ">";
+                    line += "<" + list.at(i).trimmed() + ">";
                 }
             }
             line += string;
             if( list.size() > 3 ) {
                 for( int i=list.size()-1; i>=3; --i ) {
-                    line += "</" + list.at(i) + ">";
+                    line += "</" + list.at(i).trimmed() + ">";
                 }
             }
             line += "</div>";
